ccdb2f5ad72965cf5b548c14f0a520bda97a872e
mixown2 <mixown@gmail.com>
2022-11-05 00:35:52 +0300

    add necessary files

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -564,7 +564,7 @@ class TestRectangle_update_args(unittest.TestCase):
 
     def test_update_args_five(self):
         r = Rectangle(10, 10, 10, 10, 10)
-[O        r.update(89, 2, 3, 4, 5)
+        r.update(89, 2, 3, 4, 5)
         self.assertEqual("[Rectangle] (89) 4/5 - 2/3", str(r))
 
     def test_update_args_more_than_five(self):
